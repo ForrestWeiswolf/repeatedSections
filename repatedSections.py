df4c34bc55d3df5b182a0849cf587f4087df53bd
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-11-11 17:16:02 -0500

    Uses argparse

--- repatedSections.py
+++ repatedSections.py
@@ -1,6 +1,13 @@
 import math
-import sys
-import re
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("filename", help="Name of file to count repeated sections in")
+parser.add_argument("-l", "--limit", help="Maximum number of results to display", type=int)
+parser.add_argument("-m", "--min", help="Minimum length of sequences to consider", type=int)
+
+args = parser.parse_args()
 
 def countSectionRepeats(str, minLength, sectionCounts={}):
   for length in range(minLength, math.ceil(len(str) / 2)):
@@ -9,10 +16,10 @@ def countSectionRepeats(str, minLength, sectionCounts={}):
   
   return {sect: sectionCounts[sect] for sect in sectionCounts if sectionCounts[sect] > 1}
 
-fileText = open(sys.argv[1]).read()
-repeatedSections = countSectionRepeats(fileText, int(sys.argv[2]))
+fileText = open(args.filename).read()
+repeatedSections = countSectionRepeats(fileText, args.min)
 repeatedSections = [(sect, repeatedSections[sect]) for sect in repeatedSections]
 repeatedSections = sorted(repeatedSections, key=lambda s: len(s[0]) * repeatedSections[s[1]])
 
-for i in range(min(len(repeatedSections), int(sys.argv[3]))):
+for i in range(min(len(repeatedSections), args.limit)):
   print(repeatedSections[i][0])

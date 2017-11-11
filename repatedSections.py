f115b983ce7781c3fa753a1964b05b25880d2f76
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-11-11 16:27:54 -0500

    Can be used from command-line, taking a file name and min length and printing a list of repeated sections sorted by length times number of occurences

--- repatedSections.py
+++ repatedSections.py
@@ -1,4 +1,5 @@
 import math
+import sys
 import re
 
 def countSectionRepeats(str, minLength, sectionCounts={}):
@@ -7,3 +8,11 @@ def countSectionRepeats(str, minLength, sectionCounts={}):
       sectionCounts[str[i:i+length]] = sectionCounts.get(str[i:i+length], 0) + 1
   
   return {sect: sectionCounts[sect] for sect in sectionCounts if sectionCounts[sect] > 1}
+
+fileText = open(sys.argv[1]).read()
+repeatedSections = countSectionRepeats(fileText, int(sys.argv[2]))
+repeatedSections = [(sect, repeatedSections[sect]) for sect in repeatedSections]
+repeatedSections = sorted(repeatedSections, key=lambda s: len(s[0]) * repeatedSections[s[1]])
+
+for i in range(len(repeatedSections)):
+  print(repeatedSections[i][0])

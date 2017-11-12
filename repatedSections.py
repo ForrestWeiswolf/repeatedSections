e8aadee139755f45e6fb6a550fa6c7026936a5af
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-11-12 11:33:31 -0500

    Flags to ignore whitespace or non-alpha chars

--- repatedSections.py
+++ repatedSections.py
@@ -1,11 +1,15 @@
 import math
 import argparse
+import re
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("filename", help="Name of file to count repeated sections in")
 parser.add_argument("-l", "--limit", help="Maximum number of results to display", type=int)
 parser.add_argument("-m", "--min", help="Minimum length of sequences to consider", type=int)
+parser.add_argument("-w", action='store_true', help="Ignore whitespace")
+parser.add_argument("-c", action='store_true', help="Ignore case")
+parser.add_argument("-a", action='store_true', help="Ignore non-alphanumeric characters")
 
 args = parser.parse_args()
 
@@ -17,6 +21,16 @@ def countSectionRepeats(str, minLength, sectionCounts={}):
   return {sect: sectionCounts[sect] for sect in sectionCounts if sectionCounts[sect] > 1}
 
 fileText = open(args.filename).read()
+
+toRemove = ''
+if(args.w):
+  toRemove += '\s'
+if(args.a):
+  toRemove += ("\W")
+if(len(toRemove) > 0):
+  toRemove = re.compile('[' + toRemove + ']')
+  fileText = toRemove.sub('', fileText)
+
 repeatedSections = countSectionRepeats(fileText, args.min)
 repeatedSections = [(sect, repeatedSections[sect]) for sect in repeatedSections]
 repeatedSections = sorted(repeatedSections, key=lambda s: len(s[0]) * repeatedSections[s[1]])
